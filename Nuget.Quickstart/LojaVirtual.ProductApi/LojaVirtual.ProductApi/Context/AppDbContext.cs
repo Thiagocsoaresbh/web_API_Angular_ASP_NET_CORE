401a876960bbfe5c32dd8326e16144ab0fed2068
ThiagoSoares <thiagocsoares@gmail.com>
2023-07-01 06:12:45 -0300

    Resobrescrevendo convenções para a criação do DB com a Fluent API dentro do AppDbContext e recriando o Migrations

--- Nuget.Quickstart/LojaVirtual.ProductApi/LojaVirtual.ProductApi/Context/AppDbContext.cs
+++ Nuget.Quickstart/LojaVirtual.ProductApi/LojaVirtual.ProductApi/Context/AppDbContext.cs
@@ -9,7 +9,55 @@ namespace LojaVirtual.ProductApi.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Category
+            mb.Entity<Category>().
+                Property(c => c.Name).
+                    HasMaxLength(128).
+                        IsRequired();
 
+            //Product
+            mb.Entity<Product>().
+                Property(c => c.Name).
+                    HasMaxLength(128).
+                        IsRequired();
 
+            mb.Entity<Product>().
+                Property(c => c.Description).
+                    HasMaxLength(255).
+                        IsRequired();
+
+            mb.Entity<Product>().
+                Property(c => c.ImageUrl).
+                    HasMaxLength(255).
+                        IsRequired();
+
+            mb.Entity<Product>().
+                Property(c => c.Price).
+                    HasPrecision(18, 2);
+
+            mb.Entity<Category>().
+                HasMany(g => g.Products).
+                    WithOne(c => c.Category).
+                        IsRequired().
+                            OnDelete(DeleteBehavior.Cascade);
+            mb.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    Name = "Material Escolar",
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    Name = "Acessórios",
+                }
+
+            );
+
+        }
     }
 }
